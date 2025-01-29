e4b33b4a7c5e11719ad9dd8175911111f2c8e238
Patina Makasheva <patinamakasheva@gmail.com>
2025-01-29 20:45:24 +0300

    updated pom and commented out WSC

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ package ru.skypro.homework.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.header.writers.ContentSecurityPolicyHeaderWriter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import ru.skypro.homework.dto.Role;
 
@@ -18,8 +20,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class WebSecurityConfig {
+
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**",
+            "/swagger-ui.html",
             "/swagger-ui/**",
             "/v3/api-docs",
             "/webjars/**",
@@ -32,44 +36,29 @@ public class WebSecurityConfig {
         UserDetails user =
                 User.builder()
                         .username("user@gmail.com")
-                        .password(passwordEncoder.encode("password"))
+                        .password("password")
+                        .passwordEncoder(passwordEncoder::encode)
                         .roles(Role.USER.name())
                         .build();
         return new InMemoryUserDetailsManager(user);
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // Настройка CSRF
-        CsrfTokenRequestAttributeHandler csrfTokenRequestHandler = new CsrfTokenRequestAttributeHandler();
-        csrfTokenRequestHandler.setCsrfRequestAttributeName("_csrf");
-
-        RequestMatcher csrfRequestMatcher = (request) -> {
-            // Разрешить запросы на пути из AUTH_WHITELIST без CSRF защиты
-            for (String path : AUTH_WHITELIST) {
-                if (request.getRequestURI().startsWith(path)) {
-                    return false;
-                }
-            }
-            // Включить CSRF защиту для всех остальных запросов
-            return true;
-        };
-
-        http
-                .authorizeHttpRequests(authorization ->
-                        authorization
-                                .requestMatchers(AUTH_WHITELIST).permitAll()
-                                .requestMatchers("/ads/**", "/users/**").authenticated()
-                )
-                .httpBasic(withDefaults())
-                .csrf(csrf -> csrf
-                        .csrfTokenRequestHandler(csrfTokenRequestHandler)
-                        .requireCsrfProtectionMatcher(csrfRequestMatcher)
-                )
-                .cors(cors -> cors.disable()); // Отключение CORS
-
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(authorization ->
+//                        authorization
+//                                .requestMatchers(AUTH_WHITELIST).permitAll()
+//                                .requestMatchers("/ads/**", "/users/**").authenticated()
+//                )
+//                .cors(withDefaults())  // Настройка CORS
+//                .httpBasic(AbstractHttpConfigurer::disable)  // Настройка базовой аутентификации
+//                .headers(headers -> headers
+//                        .addHeaderWriter(new ContentSecurityPolicyHeaderWriter("frame-ancestors 'self'"))
+//                );
+//        return http.build();
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
