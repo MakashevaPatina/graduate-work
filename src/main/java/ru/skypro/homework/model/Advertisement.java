722a7998bc56230e5e7c75e1d440a88c33ba1e07
Patina Makasheva <patinamakasheva@gmail.com>
2025-02-03 22:33:40 +0300

    update

--- src/main/java/ru/skypro/homework/model/Advertisement.java
+++ src/main/java/ru/skypro/homework/model/Advertisement.java
@@ -3,16 +3,20 @@ package ru.skypro.homework.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Entity
 public class Advertisement {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int id;
     private String title;
     private String description;
     private Double price;
     private String image;
     @ManyToOne
     private User author;
+    @OneToMany(mappedBy = "advertisement", cascade = CascadeType.REMOVE)
+    private List<Comment> comments;
 }
--- src/main/resources/liquibase/scripts/comment.sql
+++ src/main/resources/liquibase/scripts/comment.sql
@@ -10,3 +10,34 @@ CREATE TABLE comments (
     text TEXT NOT NULL,
     ad_id INTEGER NOT NULL
 );
+
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    phone VARCHAR(50),
+    role VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE user_avatar (
+    id BIGSERIAL PRIMARY KEY,
+    file_path VARCHAR(500) NOT NULL,
+    file_size BIGINT NOT NULL,
+    media_type VARCHAR(255) NOT NULL,
+    data BYTEA,
+    user_id BIGINT UNIQUE,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE advertisement (
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DOUBLE PRECISION NOT NULL,
+    image VARCHAR(500),
+    author_id BIGINT NOT NULL,
+    CONSTRAINT fk_advertisement_user FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE
+);
